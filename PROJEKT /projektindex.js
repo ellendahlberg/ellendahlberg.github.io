a606a5c2453c941cd5deebaf3004052b1cab07d8
Ellen <ellen99dahlberg@hotmail.com>
2017-12-15 13:00:35 +0100

    3

---
+++ PROJEKT /projektindex.js
@@ -0,0 +1,20 @@
+
+$(function(){
+    $("#schysst").click(function(){
+    $("section").hide();
+    $("#schyssttext").show();
+    });
+    
+    $("#säkerhet").click(function(){
+    $("section").hide();
+    $("#säkerhettext").show();
+    });
+    
+    $("#standarder").click(function(){
+    $("section").hide();
+    $("#standardertext").show();
+    });
+    
+});
+
+
